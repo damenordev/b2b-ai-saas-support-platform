3bdcc6d0c6c37638db728303a1ebafbee226f839
damenor <damenor.develop@gmail.com>
2025-09-25 04:27:05 +0200

    Agregar soporte de autenticación con Clerk, actualizar dependencias en pnpm y package.json, y modificar la estructura de la página principal para manejar usuarios autenticados y no autenticados.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,26 @@
-import { fetchMutation, fetchQuery } from 'convex/nextjs'
+'use client'
+
+import { useQuery } from 'convex/react'
+import { SignInButton, UserButton } from '@clerk/nextjs'
 
 import { api } from '@workspace/backend/_generated/api'
+import { Authenticated, Unauthenticated } from 'convex/react'
 
-export default async function Page() {
-  const users = await fetchQuery(api.users.getMany)
+export default function Page() {
+  const users = useQuery(api.users.getMany)
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <p>WEB</p>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex flex-col gap-4">
+          <p>WEB</p>
+          <pre>{JSON.stringify(users, null, 2)}</pre>
+        </div>
+        <UserButton />
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton>Sign in</SignInButton>
+      </Unauthenticated>
+    </>
   )
 }
